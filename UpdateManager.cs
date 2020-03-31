72e9b191dcc5737aa1314db0d140f2da680a4de8
Drew Chase <dcmanproductions@gmail.com>
2020-03-31 17:06:34 -0400

    Fixed Launcher Error.

--- UpdateManager.cs
+++ UpdateManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace ChaseLabs.CLUpdate
 {
     /// <summary>
@@ -14,54 +11,110 @@ namespace ChaseLabs.CLUpdate
     /// </summary>
     public class UpdateManager
     {
+        public static void Update(string versionKey, string localVersionFilePath, string remoteVersionFilePath, string zipUrl, string zipDirectory, string unzipDirectory, string launchExecutableName, bool overwrite)
+        {
+            if (CheckForUpdate(versionKey, localVersionFilePath, remoteVersionFilePath) || !System.IO.File.Exists(System.IO.Path.Combine(unzipDirectory, launchExecutableName)))
+            {
+                System.Console.WriteLine("Update Needed!");
+                var update = Updater.Init(zipUrl, zipDirectory, unzipDirectory, launchExecutableName, overwrite);
+                using (System.Net.WebClient client = new System.Net.WebClient())
+                {
+                    if (System.IO.File.Exists(localVersionFilePath))
+                    {
+                        System.IO.File.Delete(localVersionFilePath);
+                    }
+
+                    client.DownloadFile(remoteVersionFilePath, localVersionFilePath);
+                    client.Dispose();
+                }
+                update.LaunchExecutable();
+            }
+            else
+            {
+                System.Console.WriteLine("Your Up to Date!");
+                if (new System.Diagnostics.Process() { StartInfo = new System.Diagnostics.ProcessStartInfo() { FileName = System.IO.Path.Combine(unzipDirectory, launchExecutableName) } }.Start())
+                {
+                    System.Environment.Exit(0);
+                }
+            }
+        }
+
         public static bool CheckForUpdate(string versionKey, string localVersionFilePath, string remoteVersionFilePath)
         {
             int currentRelease = ParseVersioning(ReadLocalVersion(localVersionFilePath, versionKey))[0], currentMajor = ParseVersioning(ReadLocalVersion(localVersionFilePath, versionKey))[1], currentMinor = ParseVersioning(ReadLocalVersion(localVersionFilePath, versionKey))[2];
             int remoteRelease = ParseVersioning(ReadRemoteVersion(remoteVersionFilePath, versionKey))[0], remoteMajor = ParseVersioning(ReadRemoteVersion(remoteVersionFilePath, versionKey))[1], remoteMinor = ParseVersioning(ReadRemoteVersion(remoteVersionFilePath, versionKey))[2];
 
-            if (currentRelease < remoteRelease) return true;
-            if (currentMajor < remoteMajor) return true;
-            if (currentMinor < remoteMinor) return true;
+            if (currentRelease < remoteRelease)
+            {
+                return true;
+            }
+
+            if (currentMajor < remoteMajor)
+            {
+                return true;
+            }
+
+            if (currentMinor < remoteMinor)
+            {
+                return true;
+            }
 
             return false;
         }
 
-        static int[] ParseVersioning(string version)
+        private static int[] ParseVersioning(string version)
         {
             int[] value = { 0, 0, 0 };
             for (int i = 0; i < version.Split('.').Length; i++)
             {
-                if (i == 0) int.TryParse(version.Split('.')[i], out value[i]);
-                if (i == 1) int.TryParse(version.Split('.')[i], out value[i]);
-                if (i == 2) int.TryParse(version.Split('.')[i], out value[i]);
-            }
-            return value;
-        }
-
-        static string ReadRemoteVersion(string url, string key)
-        {
-            string value = string.Empty;
-            using (var client = new System.Net.WebClient())
-            {
-                value = client.DownloadString(url);
-                foreach (string s in value.Split(Environment.NewLine.ToCharArray()[0]))
+                if (i == 0)
                 {
-                    if (s.StartsWith(key))
-                        value = s.Replace(key, "");
+                    int.TryParse(version.Split('.')[i], out value[i]);
+                }
+
+                if (i == 1)
+                {
+                    int.TryParse(version.Split('.')[i], out value[i]);
+                }
+
+                if (i == 2)
+                {
+                    int.TryParse(version.Split('.')[i], out value[i]);
                 }
             }
             return value;
         }
 
-        static string ReadLocalVersion(string path, string key)
+        private static string ReadRemoteVersion(string url, string key)
         {
             string value = string.Empty;
-            using (var reader = new System.IO.StreamReader(path))
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                value = client.DownloadString(url);
+                foreach (string s in value.Split(System.Environment.NewLine.ToCharArray()[0]))
+                {
+                    if (s.StartsWith(key))
+                    {
+                        value = s.Replace(key, "");
+                    }
+                }
+                client.Dispose();
+            }
+            return value;
+        }
+
+        private static string ReadLocalVersion(string path, string key)
+        {
+            string value = string.Empty;
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(path))
             {
                 while (!reader.EndOfStream)
                 {
                     string txt = reader.ReadLine();
-                    if (txt.StartsWith(key)) value = txt.Replace(key, "");
+                    if (txt.StartsWith(key))
+                    {
+                        value = txt.Replace(key, "");
+                    }
                 }
             }
             return value;
--- Updater.cs
+++ Updater.cs
@@ -11,9 +11,9 @@ namespace ChaseLabs.CLUpdate
     /// </summary>
     public class Updater : Interfaces.IUpdater
     {
-        string url, zipdir, unzipdir, lexe;
-        bool overwrite;
-        int progress = 0;
+        private readonly string url, zipdir, unzipdir, lexe;
+        private readonly bool overwrite;
+        private int progress = 0;
         public string URL => url;
 
         public string ZipDirectory => zipdir;
@@ -26,64 +26,98 @@ namespace ChaseLabs.CLUpdate
 
         public int DownloadProgress => progress;
 
-        Updater(string _url, string _zipDirectory, string _unzipDirectory, string _launchExecutableName, bool _overwrite)
+        private Updater(string _url, string _zipDirectory, string _unzipDirectory, string _launchExecutableName, bool _overwrite)
         {
             url = _url;
             zipdir = _zipDirectory;
             unzipdir = _unzipDirectory;
             lexe = _launchExecutableName;
             overwrite = _overwrite;
-            if (!System.IO.Directory.Exists(UnzipDirectory)) System.IO.Directory.CreateDirectory(UnzipDirectory);
-            if (!System.IO.Directory.Exists(ZipDirectory)) System.IO.Directory.CreateDirectory(ZipDirectory);
-            if (System.IO.File.Exists(System.IO.Path.Combine(ZipDirectory, "unzip.zip"))) System.IO.File.Delete(System.IO.Path.Combine(ZipDirectory, "unzip.zip"));
+
+            if (!System.IO.Directory.Exists(UnzipDirectory))
+            {
+                System.Console.WriteLine("Removing Spent Update Archive Directory");
+                System.IO.Directory.CreateDirectory(UnzipDirectory);
+            }
+
+            if (!System.IO.Directory.Exists(ZipDirectory))
+            {
+                System.Console.WriteLine("Creating Application Directory");
+                System.IO.Directory.CreateDirectory(ZipDirectory);
+            }
+
+            if (System.IO.File.Exists(System.IO.Path.Combine(ZipDirectory, "update.zip")))
+            {
+                System.Console.WriteLine("Removing Spent Update Archive");
+                System.IO.File.Delete(System.IO.Path.Combine(ZipDirectory, "update.zip"));
+            }
 
             Start();
         }
 
         public static Interfaces.IUpdater Init(string _url, string _zipDirectory, string _unzipDirectory, string _launchExecutableName, bool _overwrite)
         {
+            System.Console.WriteLine("Initializing Updater...");
             return new Updater(_url, _zipDirectory, _unzipDirectory, _launchExecutableName, _overwrite);
         }
 
-        void Start()
+        private void Start()
         {
+            System.Console.WriteLine("Starting Updater...");
             Download();
         }
 
         public void Download()
         {
-            using (var client = new System.Net.WebClient())
+            System.Console.WriteLine("Downloading Update...");
+            using (System.Net.WebClient client = new System.Net.WebClient())
             {
-                client.DownloadFile(URL, System.IO.Path.Combine(ZipDirectory, "unzip.zip"));
-                client.DownloadProgressChanged += ((object sender, System.Net.DownloadProgressChangedEventArgs e) => { progress = e.ProgressPercentage; });
                 client.DownloadFileCompleted += ((object sender, System.ComponentModel.AsyncCompletedEventArgs e) => { Unzip(); });
+                client.DownloadProgressChanged += ((object sender, System.Net.DownloadProgressChangedEventArgs e) => { System.Console.WriteLine($"{e.ProgressPercentage}%"); progress = e.ProgressPercentage; });
+                client.DownloadFile(URL, System.IO.Path.Combine(ZipDirectory, "update.zip"));
+                client.Dispose();
+                System.Threading.Thread.Sleep(2 * 1000);
+                Unzip();
             }
         }
 
         public void Unzip()
         {
+            System.Console.WriteLine("Unziping Update...");
             if (OverwriteDirectory)
             {
-                foreach (var file in System.IO.Directory.GetFiles(UnzipDirectory))
+                foreach (string file in System.IO.Directory.GetFiles(UnzipDirectory))
                 {
                     System.IO.File.Delete(file);
                 }
             }
             System.IO.Compression.ZipFile.ExtractToDirectory(System.IO.Path.Combine(ZipDirectory, "update.zip"), UnzipDirectory);
+            System.Threading.Thread.Sleep(2 * 1000);
             CleanUp();
         }
 
         public void CleanUp()
         {
-            if (System.IO.File.Exists(System.IO.Path.Combine(ZipDirectory, "unzip.zip"))) System.IO.File.Delete(System.IO.Path.Combine(ZipDirectory, "unzip.zip"));
-            if (System.IO.Directory.Exists(ZipDirectory)) System.IO.Directory.Delete(ZipDirectory);
-            LaunchExecutable();
+            System.Console.WriteLine("Cleaning Up...");
+            if (System.IO.File.Exists(System.IO.Path.Combine(ZipDirectory, "update.zip")))
+            {
+                System.IO.File.Delete(System.IO.Path.Combine(ZipDirectory, "update.zip"));
+            }
+
+            if (System.IO.Directory.Exists(ZipDirectory))
+            {
+                System.IO.Directory.Delete(ZipDirectory);
+            }
         }
 
         public void LaunchExecutable()
         {
-            if (new System.Diagnostics.Process() { StartInfo = new System.Diagnostics.ProcessStartInfo() { FileName = LaunchExecutableName, CreateNoWindow = true, UseShellExecute = false } }.Start())
+            System.Threading.Thread.Sleep(2 * 1000);
+            System.Console.WriteLine("Launching Executable...");
+            if (new System.Diagnostics.Process() { StartInfo = new System.Diagnostics.ProcessStartInfo() { FileName = System.IO.Path.Combine(UnzipDirectory, LaunchExecutableName) } }.Start())
+            {
                 System.Environment.Exit(0);
+            }
         }
 
     }
